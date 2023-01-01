aa6003b5d808d170842d91c18b99a1315e32f51f
繁星 <63717760+kelezdy@users.noreply.github.com>
2023-01-01 15:54:29 +0800

    Update index.js

--- lsck/js/index.js
+++ lsck/js/index.js
@@ -1,22 +1,54 @@
-var n = 1;
-var n1 = n + 1;
+var n = true;
 var on = false;
 document.onmousewheel = function (e) {
     if (on == false) {
         on = true;
+        let p = document.querySelector("#p");
+        var top = getComputedStyle(document.documentElement).getPropertyValue('--top');
+        top = top.slice(0, top.indexOf('v'));
+        document.documentElement.style.setProperty('--ytop', top + 'vh');
+        console.log(top + 'vh');
         if (e.wheelDelta > 0) {
-            let p = document.querySelector("#p");
-            var top = getComputedStyle(document.documentElement).getPropertyValue('--top');
-            top=top+100;
-            style.setProperty('--top', top);
-            console.log(`${top}vh`);
-            p.style['top'] = `${top}vh`;
-            p.style['animation'] = 'bg-x 1s 1';
+            top = parseInt(top) + 100;
         } else {
+            top = parseInt(top) - 100;
+        }
+        document.documentElement.style.setProperty('--top', top + 'vh')
+        console.log(top + 'vh');
+        if (n == true) {
+            n = false
+            p.style['animation'] = 'bg-s 0.9s 1 forwards';
+        } else {
+            n = true
+            p.style['animation'] = 'bg-x 0.9s 1 forwards';
         }
         setTimeout(function () {
             on = false;
-        }, 1000);
+        }, 900);
+    }
+}
+
+function p1on() {
+    if (on == false) {
+        on = true;
+        let p = document.querySelector("#p");
+        var top = getComputedStyle(document.documentElement).getPropertyValue('--top');
+        top = top.slice(0, top.indexOf('v'));
+        document.documentElement.style.setProperty('--ytop', top + 'vh');
+        console.log(top + 'vh');
+        top = parseInt(top) - 100;
+        document.documentElement.style.setProperty('--top', top + 'vh')
+        console.log(top + 'vh');
+        if (n == true) {
+            n = false
+            p.style['animation'] = 'bg-s 0.9s 1 forwards';
+        } else {
+            n = true
+            p.style['animation'] = 'bg-x 0.9s 1 forwards';
+        }
+        setTimeout(function () {
+            on = false;
+        }, 900);
     }
 }
 
